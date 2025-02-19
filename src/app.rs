4d80cc0ef8025f426a5350a9976810a21fbe9e38
Benjamin Richardson <21092217+benfuddled@users.noreply.github.com>
2025-02-18 23:50:27 -0500

    Remove unneeded function

--- src/app.rs
+++ src/app.rs
@@ -1534,18 +1534,3 @@ fn print_tag_item(idx: usize, key: &str, value: &Value, indent: usize) -> String
 
     out
 }
-
-fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()> {
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                visit_dirs(&path, cb)?;
-            } else {
-                cb(&entry);
-            }
-        }
-    }
-    Ok(())
-}
